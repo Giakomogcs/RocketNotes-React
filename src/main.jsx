518622e12cfb609f9c704cbf9b77014073ae0a89
Giovani <giakomogcs@gmail.com>
2023-02-23 20:25:43 -0300

    Estilo Global

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Details} from './pages/Details'
 
+import { ThemeProvider } from 'styled-components'
+import theme from './styles/theme'
+
+import GlobalStyle from './styles/global'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Details />
+    <ThemeProvider  theme={theme}>
+      <GlobalStyle/>
+      <Details />
+    </ThemeProvider>
   </React.StrictMode>,
 )

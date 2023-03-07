3e95dc43d7207d5d9b0d4f0d8e46e32fe8215fdc
Giovani <giakomogcs@gmail.com>
2023-03-06 22:38:01 -0300

    Section

--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -9,4 +9,15 @@ export const Container = styled.div`
   grid-template-areas: 
   "header"
   "content";
-`
+`;
+
+export const Links = styled.ul`
+  list-style: none;
+  > li{
+    margin-top: 12px;
+
+    a{
+      color:  ${({theme}) => theme.COLORS.WHITE};
+    }
+  }
+`;

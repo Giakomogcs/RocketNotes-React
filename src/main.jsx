0a707cad9974c5f3b802d32708a3f0a7aad9985d
Giovani <giakomogcs@gmail.com>
2023-03-13 20:43:55 -0300

    SignUp

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import {Details} from './pages/Details'
 import { Home } from './pages/Home'
 import {SignIn} from './pages/SignIn'
+import { SignUp } from './pages/SignUp'
 
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
@@ -14,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider  theme={theme}>
       <GlobalStyle/>
-      <SignIn />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>,
 )
--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -1,11 +1,13 @@
 import styled from "styled-components"
 import backgroungImg from '../../assets/background.png'
+
 export const Container = styled.div`
   height: 100vh;
 
   display: flex;
   align-items: stretch;
 `;
+
 
 export const Form = styled.form`
   padding: 0 136px;

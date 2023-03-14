784ecbff606822c9021ef6e53d1bc3c17ae68cad
Giovani <giakomogcs@gmail.com>
2023-03-13 22:24:04 -0300

    header New notes

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import { Home } from './pages/Home'
 import {SignIn} from './pages/SignIn'
 import { SignUp } from './pages/SignUp'
 import { Profile } from './pages/Profile'
+import { New } from './pages/New'
 
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
@@ -16,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider  theme={theme}>
       <GlobalStyle/>
-      <Profile />
+      <New />
     </ThemeProvider>
   </React.StrictMode>,
 )

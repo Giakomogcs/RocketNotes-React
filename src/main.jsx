07a0c389c1cd409544bf10de16bf3217d8d92473
Giovani <giakomogcs@gmail.com>
2023-03-15 10:38:22 -0300

    Rotes

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {Details} from './pages/Details'
-import { Home } from './pages/Home'
-import {SignIn} from './pages/SignIn'
-import { SignUp } from './pages/SignUp'
-import { Profile } from './pages/Profile'
-import { New } from './pages/New'
+
+import { Routes } from './Routes'
 
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
@@ -17,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider  theme={theme}>
       <GlobalStyle/>
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )

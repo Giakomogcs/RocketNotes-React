a35d125ddab175368e4c895d0e3fa6d6f0b83f63
Giovani <giakomogcs@gmail.com>
2023-03-29 22:09:47 -0300

    Atualizar perfil do usuário

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -16,8 +16,12 @@ function AuthProvider({children}) {
       localStorage.setItem("@rocketnotes:user", JSON.stringify(user))
       localStorage.setItem("@rocketnotes:token", token)
 
-      api.defaults.headers.common["authorization"] = `Bearer ${token}`
-      setData({user, token})
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+
+      setData({
+        user, 
+        token
+      })
 
     } catch (error) {
       
@@ -37,12 +41,33 @@ function AuthProvider({children}) {
     setData({})
   }
 
+  async function updateProfile({user}) {
+
+    try {
+      await api.put("/users", user)
+      localStorage.setItem("@rocketnotes:user", JSON.stringify(user))
+
+      setData({
+        user, 
+        token: data.token
+      })
+      alert("Perfil atualizado!")
+
+    } catch (error) {
+      if(error.response){
+        alert(error.response.data.message)
+      }else{
+        alert("Não foi possível atualizar esse perfil")
+      }
+    }
+  }
+
   useEffect(() => {
     const token = localStorage.getItem("@rocketnotes:token")
     const user = localStorage.getItem("@rocketnotes:user")
 
     if(token && user) {
-      api.defaults.headers.common["authorization"] = `Bearer ${token}`
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 
       setData({
         token,
@@ -55,6 +80,7 @@ function AuthProvider({children}) {
     <AuthContext.Provider value={{
       signIn, 
       signOut, 
+      updateProfile,
       user: data.user
     }}
     >

16060a30fdab334895f6bbe28e587a8ccd175258
Giovani <giakomogcs@gmail.com>
2023-03-31 21:18:57 -0300

    Carregando tags e notas

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,8 +5,25 @@ import {ButtonText} from '../../components/ButtonText'
 import { Input } from '../../components/Input'
 import {Section} from '../../components/Section'
 import {Note} from '../../components/Note'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+import { api } from '../../services/api'
 
 export function Home(){
+
+  const [tags, setTags] = useState([])
+
+  
+  useEffect(() => {
+    async function fetchTags(){
+      const response = await api.get("/tags")
+      setTags(response.data)
+    }
+    
+    fetchTags()
+  }, [])
+
   return(
     <Container>
       <Brand>
@@ -16,9 +33,21 @@ export function Home(){
       <Header/>
 
       <Menu>
-        <li><ButtonText title='Todos' isActive/></li>
-        <li><ButtonText title='React'/></li>
-        <li><ButtonText title='Node'/></li>
+        <li>
+          <ButtonText 
+            title='Todos' 
+            isActive
+          />
+        </li>
+        {
+          tags && tags.map(tag => (
+            <li key={String(tag.id)}>
+              <ButtonText 
+                title={tag.name} 
+              />
+            </li>
+          ))
+        }
       </Menu>
 
       <Search>
